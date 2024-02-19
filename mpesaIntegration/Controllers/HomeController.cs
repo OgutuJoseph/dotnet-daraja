0112af1b23d6847f90bf2b8cdb120e3f1719afb6
Joseph Ogutu (KE) <onyango.joseph@yahoo.com>
2024-02-19 11:45:22 +0300

    Section 01: Create Mpesa Client

--- mpesaIntegration/Controllers/HomeController.cs
+++ mpesaIntegration/Controllers/HomeController.cs
@@ -23,7 +23,10 @@ public class HomeController : Controller
 
     public async Task<string> GetToken() 
     {
-        
+        var client = _clientFactory.CreateClient("mpesa");
+        var authString = "ITNqYY0yJE0qF1hVbug5PmD0y6MdFWBG:IzCv1CjMkrrYI3Py";
+        var encodedString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authString));
+        var url = "/oauth/v1/generate?grant_type=client_credentials";
     }
 
     public IActionResult Privacy()
